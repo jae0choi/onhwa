5107cc19e625e6432882d18fc83ba1337ff4c848
Jaeyoung Choi <jychoi84@gmail.com>
2021-03-13 14:02:20 -0800

    global playlist add/removal

--- api/api.py
+++ api/api.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import jsonify
+from flask import request  
 
 from forms import Form
 
@@ -16,6 +17,8 @@ app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
+playlist = []
+
 @app.route('/', methods=['GET'])
 def main():
     form = Form()
@@ -27,8 +30,20 @@ def search_youtube():
     q = form.query.data
     return jsonify(data=youtube_search(q))
 
-#@app.route('/addSong', methods=['GET', 'POST'])
-#def addSong():
+@app.route('/load_playlist', methods=['GET'])
+def load_playlist():
+    return jsonify(data=playlist)
 
-    
+@app.route('/edit_playlist', methods=['GET', 'POST'])
+def edit_playlist():
+    global playlist
+    vid = request.form['video_id']
+    mode = request.form['mode']
+    if mode == 'add':
+        playlist.append(vid)
+    elif mode == 'remove':
+        playlist.remove(vid)
+    pp.pprint(playlist)
+    return jsonify(data=playlist)
 
+#@app.route('/remove_from_playlist', methods=['GET', 'POST'])
